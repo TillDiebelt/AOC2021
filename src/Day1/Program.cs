71ae9a12c524e2002a11fb0b19fdd9bea9e93c1d
Till Diebelt <till.diebelt@stud.htwk-leipzig.de>
2021-12-01 15:34:10 +0100

    only one methode for both parts

--- src/Day1/Program.cs
+++ src/Day1/Program.cs
@@ -20,14 +20,14 @@ namespace Day1
             Console.WriteLine("=================================================================================================");
             Console.WriteLine("                                       AOC 2021 Day " + day + "                                  ");
             Console.WriteLine("=================================================================================================");
-            (long parsing, long calculation, int solution) runtimePart1 = SolvePart1(inputPath);
+            (long parsing, long calculation, int solution) runtimePart1 = SolveBoth(inputPath, 1);
             Console.WriteLine("Solution Part 1:");
             Console.WriteLine(runtimePart1.solution);
             Console.WriteLine("Solution for day " + day + " part 1 took: " + runtimePart1.parsing + " ticks to parse and " 
                 + runtimePart1.calculation + " ticks to calculate");
             Console.WriteLine();
 
-            (long parsing, long calculation, int solution) runtimePart2 = SolvePart2(inputPath);
+            (long parsing, long calculation, int solution) runtimePart2 = SolveBoth(inputPath, 3);
             Console.WriteLine("Solution Part 2:");
             Console.WriteLine(runtimePart2.solution);
             Console.WriteLine("Solution for day " + day + " part 1 took: " + runtimePart2.parsing + " ticks to parse and " 
@@ -35,7 +35,7 @@ namespace Day1
 
         }
 
-        private static (long parsing, long calculation, int solution) SolvePart1(string inputPath)
+        private static (long parsing, long calculation, int solution) SolveBoth(string inputPath, int distance)
         {
             Stopwatch stopwatch = new Stopwatch();
             int result = 0;
@@ -50,34 +50,9 @@ namespace Day1
             stopwatch.Restart();
 
             //calculation
-            for (int i = 1; i < depths.Length; i++)
+            for (int i = 0; i < depths.Length - distance; i++)
             {
-                result += depths[i - 1] < depths[i] ? 1 : 0;
-            }
-
-            stopwatch.Stop();
-            return (parseTime,stopwatch.ElapsedTicks, result);
-        }
-
-        private static (long parsing, long calculation, int solution) SolvePart2(string inputPath)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            int result = 0;
-            string input = File.ReadAllText(inputPath);
-            stopwatch.Start();
-
-            //parsing
-            int[] depths = Array.ConvertAll(input.Split("\n"), int.Parse);
-
-            stopwatch.Stop();
-            long parseTime = stopwatch.ElapsedTicks;
-            stopwatch.Restart();
-
-            //calculation
-            for (int i = 0; i < depths.Length-3; i++)
-            {
-                //result += (depths[i..(i + 3)].Sum() < depths[(i + 1)..(i + 4)].Sum()) ? 1 : 0;
-                result += (depths[i] < depths[i+3]) ? 1 : 0;
+                result += depths[i] < depths[i + distance] ? 1 : 0;
             }
 
             stopwatch.Stop();
