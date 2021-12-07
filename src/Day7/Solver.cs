c96467f8fc4ae0a9f34dca15a1155beaca28bd75
Till Diebelt <till.diebelt@stud.htwk-leipzig.de>
2021-12-07 07:36:18 +0100

    alternative day 7 part 1

--- src/Day7/Solver.cs
+++ src/Day7/Solver.cs
@@ -19,5 +19,31 @@ namespace Day7
 
             return costs.Min();
         }
+
+        public static long SolvePart1Alternitive(string inputPath, Func<int, int> cost)
+        {
+            string input = File.ReadAllText(inputPath).Replace("\r", "");
+            var crabs = input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x));
+            int count = crabs.Count();
+            var ordered = crabs.OrderBy(p => p);
+            float median = ordered.ElementAt(count / 2) + ordered.ElementAt((count - 1) / 2);
+            median /= 2;
+            var index = (int)Math.Floor(median);
+            long result = 0;
+            foreach (var crab in crabs)
+                result += cost(Math.Abs(index - crab));
+            return result;
+        }
+
+        public static long SolvePart2Alternitive(string inputPath, Func<int, int> cost)
+        {
+            string input = File.ReadAllText(inputPath).Replace("\r", "");
+            var crabs = input.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Convert.ToInt32(x));
+            var index = (int)Math.Floor(crabs.Average());
+            long result = 0;
+            foreach (var crab in crabs)
+                result += cost(Math.Abs(index - crab));
+            return result;
+        }
     }
 }
